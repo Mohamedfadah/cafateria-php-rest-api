be241dfb0e21abd6332e9332162e8e7b29f7c027
Mohammed Atef <mohammed.eldegwy2016@gmail.com>
2022-04-12 13:32:48 +0200

    delete operation

---
+++ api/v1/client/get_one.php
@@ -0,0 +1,35 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../../config/Database.php';
+    include_once '../../../models/Client.php';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $db = new Database();
+        $db = $db->connect();
+
+        $client = new Client($db);
+
+        $data = json_decode(file_get_contents("php://input"));
+
+        if (isset($data->id)) {
+            $client->id = $data->id;
+
+            if ($client->fetchOne()) {
+                print_r(json_encode(array(
+                    'id' => $client->id,
+                    'name' => $client->name,
+                    'username' => $client->username,
+                    'pass' => $client->pass,
+                    'email' => $client->email
+                )));
+            } else {
+                echo json_encode(array('message' => "No records found!"));
+            }
+        } else {
+            echo json_encode(array('message' => "Error: Student ID is missing!"));
+        }
+    } else {
+        echo json_encode(array('message' => "Error: incorrect Method!"));
+    }
---
+++ models/Client.php
@@ -0,0 +1,89 @@
+<?php
+
+class Client
+{
+    private $conn;
+    
+    public $id;
+    public $name;
+    public $address;
+    public $age;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function fetchAll()
+    {
+        $stmt = $this->conn->prepare('SELECT * FROM client');
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function fetchOne()
+    {
+        $stmt = $this->conn->prepare('SELECT  * FROM client WHERE id = ?');
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $this->id = $row['id'];
+            $this->name = $row['name'];
+            $this->address = $row['username'];
+            $this->pass = $row['pass'];
+            $this->email = $row['email'];
+
+            return true;
+        }
+        
+        return false;
+    }
+
+    public function postData()
+    {
+        $stmt = $this->conn->prepare('INSERT INTO client SET name = :name, username = :username, pass = :pass, email = :email');
+
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':username', $this->username);
+        $stmt->bindParam(':pass', $this->pass);
+        $stmt->bindParam(':email', $this->email);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function putData()
+    {
+        $stmt = $this->conn->prepare('UPDATE client SET name = :name, username = :username, pass = :pass, email = :email WHERE id = :id');
+
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':username', $this->username);
+        $stmt->bindParam(':pass', $this->pass);
+        $stmt->bindParam(':email', $this->email);
+        $stmt->bindParam(':id', $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function delete()
+    {
+        $stmt = $this->conn->prepare('DELETE FROM client WHERE id = :id');
+        $stmt->bindParam(':id', $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+}
